0916c6594ed47b6e4944e9dbd69b1508cb18f7ef
Allen Lee <allenleexxx@163.com>
2026-03-03 03:42:42 +0000

    调整设计，添加REPTILE图标，更新页面标题和描述

--- src/views/widgets/watermark/index.vue
+++ src/views/widgets/watermark/index.vue
@@ -3,7 +3,7 @@
     <!-- 基础文字水印 -->
     <ElCard class="mb-7.5" shadow="never">
       <template #header>基础文字水印</template>
-      <ElWatermark content="Art Design Pro" :font="{ color: 'rgba(128, 128, 128, 0.2)' }">
+      <ElWatermark content="REPTILE DEMO" :font="{ color: 'rgba(128, 128, 128, 0.2)' }">
         <div style="height: 200px"></div>
       </ElWatermark>
     </ElCard>
@@ -12,7 +12,7 @@
     <ElCard class="mb-7.5" shadow="never">
       <template #header>多行文字水印</template>
       <ElWatermark
-        :content="['Art Design Pro', '专注用户体验，视觉设计']"
+        :content="['REPTILE DEMO', '专注用户体验，视觉设计']"
         :font="{ fontSize: 16, color: 'rgba(128, 128, 128, 0.2)' }"
       >
         <div style="height: 200px"></div>
@@ -31,7 +31,7 @@
     <ElCard class="mb-7.5" shadow="never">
       <template #header>自定义样式水印</template>
       <ElWatermark
-        content="Art Design Pro"
+        content="REPTILE DEMO"
         :font="{
           fontSize: 20,
           fontFamily: 'Arial',
